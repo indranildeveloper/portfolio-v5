b673c64db091711a7e94971727d0258fc36d1017
Indranil Halder <indranilhalder.dev@gmail.com>
2024-05-31 00:27:21 +0530

    Updated resume

--- src/sanity/sanity.config.ts
+++ src/sanity/sanity.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import schemas from "./schemas";
 
-// TODO: add schemas
 const sanityConfig = defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_APP_ID!,
   dataset: "production",
