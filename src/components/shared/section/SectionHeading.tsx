162915db32a85ed7686bbcd16cc00307f147d22f
Indranil Halder <indranilhalder.dev@gmail.com>
2024-06-21 01:26:48 +0530

    Section Heading Animations

--- src/components/shared/section/SectionHeading.tsx
+++ src/components/shared/section/SectionHeading.tsx
@@ -1,23 +1,30 @@
 "use client";
 
-import { FC, ReactNode } from "react";
-
-interface SectionHeadingProps {
-  Icon: ReactNode;
-  headingTitle: string;
-}
+import { FC } from "react";
+import { motion } from "framer-motion";
+import { SectionHeadingProps } from "@/interfaces/components/SectionHeadingProps";
+import { sectionHeadingVariants } from "@/animations/sectionHeadingVariants";
 
 const SectionHeading: FC<SectionHeadingProps> = ({ Icon, headingTitle }) => {
   return (
-    <div className="mb-10 flex items-center gap-4">
-      <div className="flex flex-1 items-center gap-2 md:flex-shrink-0 md:flex-grow-0">
-        <span>{Icon}</span>
-        <h2 className="text-wrap text-xl font-bold text-slate-700 dark:text-slate-300 md:text-nowrap md:text-3xl">
-          {headingTitle}
-        </h2>
-      </div>
-      <div className="h-2 flex-1 rounded-full bg-gradient-to-r from-primary to-indigo-600" />
-    </div>
+    <motion.div
+      initial="offscreen"
+      whileInView="onscreen"
+      viewport={{ once: true, amount: 0.8 }}
+    >
+      <motion.div
+        variants={sectionHeadingVariants}
+        className="mb-10 flex items-center gap-4"
+      >
+        <div className="flex flex-1 items-center gap-2 md:flex-shrink-0 md:flex-grow-0">
+          <span>{Icon}</span>
+          <h2 className="text-wrap text-xl font-bold text-slate-700 dark:text-slate-300 md:text-nowrap md:text-3xl">
+            {headingTitle}
+          </h2>
+        </div>
+        <div className="h-2 flex-1 rounded-full bg-gradient-to-r from-primary to-indigo-600" />
+      </motion.div>
+    </motion.div>
   );
 };
 
